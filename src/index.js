9ea85439cc553d680a6382a0e7334d1d841f4fc8
Oliver Plunkett <otplunkett@gmail.com>
2018-10-03 17:41:22 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -22,6 +22,6 @@ app.use(
     graphiql: true
   })
 )
-app.listen(process.env.PORT || 4000, () => {
-  console.log("Listening...")
-})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Listening on ${port}.`))
